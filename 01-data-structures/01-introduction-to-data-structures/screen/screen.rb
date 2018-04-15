fb66c1c996ce01975bed0c26c3800d2bed298c5f
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-04-15 15:51:48 -0700

    updated answer files

--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -1,29 +1,40 @@
 require_relative 'pixel'
 
 
-class Screen < Pixel
+class Screen
   attr_accessor :width
   attr_accessor :height
   attr_accessor :matrix
 
   def initialize(width, height)
     @width, @height = width, height
-    @matrix = [@width,@height]
-  end
-  
-  # Insert a Pixel at x, y
-  def insert(pixel, x, y)
-    @matrix[x,y] = pixel
+    # create a 2D array to hold the data
+    @matrix = Array.new(@width) {Array.new(@height)}
   end
 
+  # Insert a Pixel at x, y for valid coordinates
+  def insert(pixel, x, y)
+    if inbounds(x,y)
+      @matrix[x][y] = pixel
+    end
+  end
+
+  # extract pixel values for valid x,y coordinates
   def at(x, y)
-    @matrix[x,y]
+    if inbounds(x,y)
+      @matrix[x][y]
+    end
   end
 
   private
 
+  # determine if x,y are valid coordinates
   def inbounds(x, y)
-
+    if x <= @width && y <= @height
+      return true
+    else
+      return nil
+    end
   end
 
 end
