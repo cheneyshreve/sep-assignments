228565801fbd63f9ebc0a70d7173b895cc721643
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-04-15 15:04:26 -0700

    working on screen,pixel classes

--- 01-data-structures/01-introduction-to-data-structures/screen/pixel.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/pixel.rb
@@ -1,7 +1,12 @@
-# Pixels require three color values: red, green, and blue.
-# Pixel colors red, green, and blue values must be greater than zero and less than 255.
+# Pixels require three color color: red, green, and blue.
+# Pixel colors red, green, and blue color must be greater than zero and less than 255.
 # If  a value less than zero is specified, default to zero. If a value greater than 255 is specified, default to 255.
 # Pixels also require a coordinate location: x and y.
+# Pixels require three colors: red, green, and blue.
+# Pixel colors red, green, and blue color must be greater than zero and less than 255.
+# If  a value less than zero is specified, default to zero. If a value greater than 255 is specified, default to 255.
+# Pixels also require a coordinate location: x and y.
+
 
 class Pixel
   attr_accessor :red
@@ -10,13 +15,30 @@ class Pixel
   attr_accessor :x
   attr_accessor :y
 
-
   def initialize(red, green, blue, x, y)
+    @red,@green,@blue = validate_color(red), validate_color(green), validate_color(blue)
+    @x, @y = x,y
   end
+
+  def x
+    @x
+  end
+
+  def y
+   @y
+  end
+
 
   private
 
   def validate_color(color)
+    if color < 0
+      color = 0
+    elsif color > 255
+      color = 255
+    else
+      color = color
+    end
   end
 
 end
--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -1,23 +1,29 @@
 require_relative 'pixel'
 
-class Screen
+
+class Screen < Pixel
   attr_accessor :width
   attr_accessor :height
   attr_accessor :matrix
 
   def initialize(width, height)
+    @width, @height = width, height
+    @matrix = [@width,@height]
   end
-
+  
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
+    @matrix[x,y] = pixel
   end
 
   def at(x, y)
+    @matrix[x,y]
   end
 
   private
 
   def inbounds(x, y)
+
   end
 
 end
