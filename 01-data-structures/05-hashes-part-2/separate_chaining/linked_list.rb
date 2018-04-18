80ac9e724500854f658f4ccdd1745809e8eb0bd0
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-04-18 15:54:11 -0700

    updated separate chaining

--- 01-data-structures/05-hashes-part-2/separate_chaining/linked_list.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/linked_list.rb
@@ -4,46 +4,41 @@ class LinkedList
   attr_accessor :head
   attr_accessor :tail
 
-
   # This method creates a new `Node` using `data`, and inserts it at the end of the list.
   def add_to_tail(node)
-    # set head to temporary variable, current
     current = @head
-    # if list is empty [], add the node
     if (@head == nil) && (@tail == nil)
       @head = node
       @tail = node
-      @head.next = nil
     else
-    # we have something like this [head|tail] -> [head|tail] -> [head|nil]
-    # iterate over the list until you come to the end, e.g. pointer equals nil
-    while current.next != nil
-      current = current.next
-    end
-    # now that we have the last node [head|nil], assign it to the input node
-      current.head = node
+      while current.next != nil
+        current = current.next
+      end
+      current.next = node
       @tail = node
     end
   end
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
-    # iterate over list until you find tail, e.g. head.next = nil
     current = @head
-    until current.next == nil
-      current.next
+    return nil if @tail == nil
+    if @head == @tail
+      @head = nil
+      @tail = nil
+    else
+      while (current.next != nil) && (current.next != @tail)
+        current = current.next
+      end
+        @tail = current
+        @tail.next = nil
     end
-    # set the new tail to be the current node, define new "end"
-    @tail = current
-    @tail.next = nil
   end
 
   # This method prints out a representation of the list.
   def print
-    # if we have no list, return an empty string
     current = @head
     return "" if @head == nil
-    # traverse the nodes, print the values
     until current.next == nil
       current = current.next
       puts current.data
@@ -54,43 +49,31 @@ class LinkedList
   # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
    current = @head
-   # if node is empty, return nil, invalid request
-   return nil if current == nil
-   # check if current node is the only node [head|tail]
+   return nil if @tail == nil
    if (@head == @tail)
      @head = nil
      @tail = nil
    elsif (@head == node)
-     # if the current head is node, set the new head to next position
      @head = @head.next
-  # if node is tail, remove the tail
    elsif (@tail == node)
      remove_tail
-    # otherwise, traverse nodes, [head|tail] -> [head|tail] != [head|nil]
    else
      while (current.next != node) && (current.next != nil)
        current = current.next
      end
-     # here's the tricky part, because we're deleting a node,
-     # [head|tail] -> [head|tail] -> [head|tail], say we're deleting second node
-     # now first [head|tail] needs to point to where the old tail pointed, so next.next
      current.next = current.next.next
    end
   end
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
   def add_to_front(node)
-    # if it's empty, add the node
     if (@head == nil) && (@tail == nil)
       @head = node
       @head.next = nil
       @tail = node
     else
-      # set the oldHead to a temp variable
       temp = @head
-      # set the new node as the head
       @head = node
-      # now bump the oldHead to the next position
       @head.next = temp
     end
 
@@ -98,23 +81,11 @@ class LinkedList
 
   # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
   def remove_front
-    # if we don't have a head, or we're at the end, or there's only one node, set it to nil
     if (@head == nil) || (@head.next == nil) || (@head == @tail)
       @head = nil
     else
-      # just set the original head to be the next node
       @head = @head.next
     end
-  end
-
-# new methods
-  def find_node(key)
-
-
-  end
-
-  def size
-
   end
 
 end
--- 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
@@ -1,79 +1,76 @@
 require_relative 'linked_list'
+require_relative 'node'
 
 class SeparateChaining
   attr_reader :max_load_factor
 
   def initialize(size)
     @max_load_factor = 0.7
-    @lists = Array.new(size)
+    @items = Array.new(size)
+    @entries = 0
   end
 
   def []=(key, value)
-   # create a new node, assign an index
-    node = Node.new(key,value)
-    i = index(key, self.size)
-    # check conditions, list empty?
-    if @lists[i].nil?
-      # create a new linked list
-      list = Linked.new(key, self.size)
-      list.add_to_tail(node)
-      @lists[i] = list
-    # check if list is at capacity yet, if not add_to_tail
-    elsif @lists[i].size < 5
-      @lists[i].add_to_tail(node)
-    # resize if the max_load_factor is exceeded
-    else
-      if self.load_factor > max_load_factor
-        resize
-      end
+    i = self.index(key, size)
+    if @items[i] == nil
+      @items[i] = LinkedList.new
     end
-
-
+    @items[i].add_to_tail(Node.new(key,value))
+    @entries += 1
+    if load_factor > @max_load_factor
+      self.resize
+    end
   end
 
   def [](key)
-    # generate an index
-    i = index(key,self.size)
-    # check conditions
-    if @lists[i] != nil
-      node = @lists[i].find_node(key)
-      return node.value unless node.value.nil?
+    i = self.index(key,size)
+    if @items[i]
+      current = @items[i].head
+      while current != nil
+        if current.key == key
+          return current.value
+        else
+          current = current.next
+        end
+      end
     end
-
   end
 
-  # Returns a unique, deterministically reproducible index into an array
-  # We are hashing based on strings, let's use the ascii value of each string as
-  # a starting point.
   def index(key, size)
     sum = 0
     key.chars.each do |x|
       sum += x.ord
-    end  
+    end
     sum % size
   end
 
   # Calculate the current load factor
   def load_factor
-    # number of array elements in hash table / number of buckets allocated
-    count = 0.0
-    @lists.each do |x|
-     if @lists[x] != nil
-       count += @lists[x].size
-     end
-    end
-    (count/self.size)
+    @entries / self.size.to_f
   end
 
   # Simple method to return the number of items in the hash
   def size
-    @lists.length
+    @items.length
   end
 
   # Resize the hash
   def resize
-    original_values = @lists.compact
-    @lists = Array.new(self.size * 2)
-    original_values.each {|node| self[node.key] = node.value }
+    new_size = size*2
+    new_items = Array.new(new_size)
+    (0..@items.size-1).each do |i|
+      list = @items[i]
+      if list != nil
+        curr = list.head
+        new_index = index(curr.key, new_items.size)
+        while curr != nil
+          list = LinkedList.new
+          list.add_to_tail(curr)
+          new_items[new_index] = list
+          curr = curr.next
+        end
+      end
+    end
+    @items = new_items
   end
 end
