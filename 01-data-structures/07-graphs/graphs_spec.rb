77d8fc49605078098f473bdac55d482ee1af2687
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-04-24 16:54:44 -0700

    updated rspecs

--- 01-data-structures/07-graphs/graphs_spec.rb
+++ 01-data-structures/07-graphs/graphs_spec.rb
@@ -1,20 +1,56 @@
-include Rspec
-
+include RSpec
 require_relative 'node'
+require_relative 'kevin_bacon'
 
-RSpec.describe Movies, type: Class do
+RSpec.describe KevinBacon, type: Class do
 
-describe "#find_kevin_bacon" do
-  it "returns an array of at most 6 films connecting the actor to Kevin Bacon" do
+let(:root) { Node.new('Johnny Depp') }
+let(:tom_kemp) { Node.new('Tom Kemp') }
+let(:dakota_johnson) { Node.new('Dakota Johnson') }
+let(:keira_knightley) { keira_knightley = Node.new('Keira Knightley') }
+let(:elizabeth_allen) { Node.new('Elizabeth Allen') }
+let(:tree) { KevinBacon.new(root) }
 
-  end
-
-  it "returns degree connections properly" do
-  
-
-  end
+before do
+  tree.insert(root, tom_kemp)
+  tree.insert(root,dakota_johnson)
+  tree.insert(root, keira_knightley)
+  tree.insert(root, elizabeth_allen)
 end
 
+describe "#insert" do
+ it "inserts a new actor node" do
+   node = tree.find(root, "Tom Kemp")
+   expect(node.name).to eq "Tom Kemp"
+ end
+end
 
+describe "#find" do
+ it "returns actor node" do
+   node = tree.find(root, "Keira Knightley")
+   expect(node.name).to eq "Keira Knightley"
+ end
+end
+
+describe "#find_kevin_bacon for himself" do
+   specify {
+    expected_output = "Kevin Bacon has a 0 degree connection to himself."
+    expect { expect { tree.find_kevin_bacon(root,"Kevin Bacon") }.to output(expected_output).to_stdout}
+   }
+end
+
+describe "#find_kevin_bacon for 1st degree connection" do
+   specify {
+    expected_output = "Dakota Johnson is connected to Kevin Bacon through [Black Mass]."
+    expect { expect { tree.find_kevin_bacon(root,"Dakota Johnson") }.to output(expected_output).to_stdout}
+   }
+end
+
+describe "#find_kevin_bacon for a 3rd degree connection" do
+  specify {
+   expected_output = "Elizabeth Allen is connected to Kevin Bacon through [\"\Dead Man's Shoes", "The Three Weird Sisters\"\]"
+   expect { expect { tree.find_kevin_bacon(root,"Elizabeth Allen") }.to output(expected_output).to_stdout}
+  }
+end
 
 end
--- 01-data-structures/07-graphs/kevin_bacon.rb
+++ 01-data-structures/07-graphs/kevin_bacon.rb
@@ -1,22 +1,12 @@
 require_relative 'node'
 
-
-
-
 class KevinBacon
 
   def initialize(root)
     @root = root
-    @bacon = {
-      "bacon_name"=> "Kevin Bacon",
-      "collaborators"=> ['Johnny Depp','Tom Kemp','Dakota Johnson',
-      'Luke Wilson','Benedict Cumberbatch','Keith David',
-      'Aaron White','Roddy McDowall','Tom Hogan'],
-      "degree"=> 0
-    }
-
   end
 
+# inserts a new actor node
   def insert(root,node)
     if @root.nil?
       @root = node
@@ -25,6 +15,7 @@ class KevinBacon
     end
   end
 
+# finds actor node in root given valid actor name
   def find(root, actor_name)
     return nil if @root.nil? || actor_name.nil?
     actors = [@root]
@@ -36,9 +27,9 @@ class KevinBacon
         return nil
       end
     end
-
   end
 
+# calls helper method to calculate degrees of bacon from database
   def find_kevin_bacon(root,actor_name)
     return nil if root.nil? || actor_name.nil?
      actor = find(root,actor_name)
@@ -46,115 +37,99 @@ class KevinBacon
      populate_hash(actor.name)
   end
 
+# looks for self and first degree relationships to bacon. If none found, examines
+# films 1st degree actors have been in to see if they match target. If no match found,
+# examines films target's co-actors have been in to see if there is a match between that actor
+# and Kevin Bacon. Returns array of movies connecting actor to Bacon or response message
+# if no connections are found and all possibilities are exhausted.
   def populate_hash(actor_name)
-    database = {
-   "Black Mass"=> ['Kevin Bacon', 'Johnny Depp', 'Tom Kemp', 'Dakota Johnson', 'Benedict Cumberbatch'],
-   "Telling Lies in America"=> ['Kevin Bacon', 'Luke Wilson'],
-   "Novocaine"=> ['Kevin Bacon','Keith David'],
-   "The Big Picture"=> ['Kevin Bacon', 'Roddy McDowall'],
-   "Frost/Nixon":['Kevin Bacon'],
-   "The Romantic"=> ['Kevin Bacon', 'Tom Hogan'],
-   "Pirates of the Caribbean: Dead Man\'s Chest" => ['Johnny Depp', 'Keira Knightley'],
-   "Ghosts of Girfriends Past"=> ['Tom Kemp', 'Jennifer Garner'],
-   "The Family Stone"=> ['Luke Wilson', 'Rachel McAdams'],
-   "Doctor Strange"=>['Benedict Cumberbatch', 'Rachel McAdams'],
-   "Mr. & Mrs. Smith"=> ['Keith David', 'Angelina Jolie'],
-   "The Three Weird Sisters"=> ['Elizabeth Allen', 'Nancy Price'],
-   "Dead Man\'s Shoes"=> ['Nancy Price', 'Roddy McDowall'],
-   "Falling From Grace"=> ['Calude Akins', 'Aaron White'],
-   "Battle for the Planet of the Apes"=> ['Claude Akins', 'Roddy McDowall'],
-  }
+      database = {
+     "Black Mass"=> ['Kevin Bacon', 'Johnny Depp', 'Tom Kemp', 'Dakota Johnson', 'Benedict Cumberbatch'],
+     "Telling Lies in America"=> ['Kevin Bacon', 'Luke Wilson'],
+     "Novocaine"=> ['Kevin Bacon','Keith David'],
+     "The Big Picture"=> ['Kevin Bacon', 'Roddy McDowall'],
+     "Frost/Nixon":['Kevin Bacon'],
+     "The Romantic"=> ['Kevin Bacon', 'Tom Hogan'],
+     "Pirates of the Caribbean: Dead Man\'s Chest" => ['Johnny Depp', 'Keira Knightley'],
+     "Ghosts of Girfriends Past"=> ['Tom Kemp', 'Jennifer Garner'],
+     "The Family Stone"=> ['Luke Wilson', 'Rachel McAdams'],
+     "Doctor Strange"=>['Benedict Cumberbatch', 'Rachel McAdams'],
+     "Mr. & Mrs. Smith"=> ['Keith David', 'Angelina Jolie'],
+     "The Three Weird Sisters"=> ['Elizabeth Allen', 'Nancy Price'],
+     "Dead Man\'s Shoes"=> ['Nancy Price', 'Roddy McDowall'],
+     "Falling From Grace"=> ['Calude Akins', 'Aaron White'],
+     "Battle for the Planet of the Apes"=> ['Claude Akins', 'Roddy McDowall'],
+    }
 
-  # check for 1st degree connections
-   database.each do |key,value|
-     if actor_name == "Kevin Bacon"
-       return "Kevin Bacon has a 0 degree connection to himself."
-     end
-     if value.include?(actor_name) && value.include?("Kevin Bacon")
-        return puts "#{actor_name} has a 1st degree connection to " +
-       "Kevin Bacon through [#{key}]."
-     end
-   end
-
-  # otherwise, find the linking films (e.g. one's that don't include Bacon)
-  all_films = database.keys
-  linking_films = []
-
-  all_films.each do |film|
-    if !database[film].include?("Kevin Bacon")
-     linking_films << film
-    end
-  end
-
-  # extract the target actor's films and coactors they've acted with
-  linking_actors = []
-  film_target = []
-  linking_films.each do |film|
-    if database[film].include?(actor_name)
-      film_target << film
-      linking_actors << database[film]
-    end
-  end
-  # flatten the array, then exclude the person in question because we are looking
-  # now for connections through the the person's co-actors
-   collabs = linking_actors.flatten
-   target_collabs = []
-   collabs.each do |x|
-     if x != actor_name
-       target_collabs << x
-     end
-   end
-
-   # look for films where that the collaborator was in with a bacon collaborator
-   bacon_coactors = @bacon["collaborators"]
-   links = []
-   match_found = false
-   i = 0
-   unless i = target_collabs.length || match_found = true
-     bacon_coactors.each do |coactor|
-       linking_films.each do |film|
-         if database[film].include?(coactor) && database[film].include?(target_collabs[i])
-           links << film
-           match_found = true
-           i += 1
-         end
+    bacon_films = []
+    # check for 1st degree connections, stash films that Bacon is in for later use
+     database.each do |key,value|
+       if value.include?("Kevin Bacon")
+         bacon_films << key
+       end
+       if actor_name == "Kevin Bacon"
+         return "Kevin Bacon has a 0 degree connection to himself."
+       end
+       if value.include?(actor_name) && value.include?("Kevin Bacon")
+          return puts "#{actor_name} is connected to Kevin Bacon " +
+         "through [#{key}]."
        end
      end
+    # otherwise, find the linking films (e.g. one's that don't include Bacon)
+    all_films = database.keys
+    linking_films = []
 
-     all = (links + film_target).uniq
-   end
+    all_films.each do |film|
+      if !database[film].include?("Kevin Bacon")
+       linking_films << film
+      end
+    end
+    # extract the target actor's films and coactors they've acted with
+    linking_actors = []
+    film_target = []
+    linking_films.each do |film|
+      if database[film].include?(actor_name)
+        film_target << film
+        linking_actors << database[film]
+      end
+    end
+    # exclude the person in question because we are looking for connections through the the person's co-actors
+     collabs = linking_actors.flatten
+     target_collabs = []
+     collabs.each do |x|
+        if x != actor_name
+         target_collabs << x
+        end
+      end
 
-     if all.nil?
-       return "Sorry, no Bacon found"
-     else
-      return puts "#{actor_name} is connected through Kevin Bacon through #{all}"
-     end
+       # look for films where that the collaborator was in with a bacon collaborator
+      bacon_coactors = []
+      bacon_films.each do |film|
+        if database[film]
+          bacon_coactors << database[film]
+        end
+      end
+
+      bacon_coactors = bacon_coactors.flatten.uniq
+
+      links = []
+      match_found = false
+      i = 0
+      # until a connection is found or collaborators are exhausted, examine potential links
+       until i > target_collabs.length || match_found == true
+         bacon_coactors.each do |coactor|
+           linking_films.each do |film|
+             if database[film].include?(coactor) && database[film].include?(target_collabs[i])
+               links << film
+               match_found = true
+               i += 1
+             end
+           end
+         end
+         all = (links + film_target).uniq
+         return "Sorry, no Bacon found." if all.nil?
+         puts "#{actor_name} is connected to Kevin Bacon through #{all}"
+       end
   end
 
-
 end
-
-
-end
-
-
-# 1st degree examples
-root = Node.new('Johnny Depp')
-tom_kemp = Node.new('Tom Kemp')
-dakota_johnson = Node.new('Dakota Johnson')
-#2nd degree examples
-keira_knightley = Node.new('Keira Knightley')
-# 3rd degree examples
-elizabeth_allen = Node.new('Elizabeth Allen')
-
-
-tree = KevinBacon.new(root)
-tree.insert(root, tom_kemp)
-tree.insert(root,dakota_johnson)
-tree.insert(root, keira_knightley)
-# tree.find(root, 'Keira Knightley')
-# tree.find(root, "Tom Kemp")
-# tree.find(root, "Dakota Johnson")
-tree.find_kevin_bacon(root,"Dakota Johnson")
-tree.find_kevin_bacon(root,"Keira Knightley")
-tree.find_kevin_bacon(root,"Elizabeth Allen")
